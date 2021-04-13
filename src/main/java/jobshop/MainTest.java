4949558cfb7d8620741a53fbe34b2295b17f6cca
RobertL31 <loicalban@laposte.net>
2021-04-13 23:03:42 +0200

    Part2 done

--- src/main/java/jobshop/MainTest.java
+++ src/main/java/jobshop/MainTest.java
@@ -35,13 +35,54 @@ public class MainTest {
             System.out.println("GANTT: " + schedule.asciiGantt());
 
             Schedule manualSchedule = new Schedule(instance);
-            // TODO: encode the same solution
-            //manualSchedule.setStartTime(....);
+            manualSchedule.setStartTime(0,0,0);
+            manualSchedule.setStartTime(0,1,3);
+            manualSchedule.setStartTime(0,2,6);
+            manualSchedule.setStartTime(1,0,6);
+            manualSchedule.setStartTime(1,1,8);
+            manualSchedule.setStartTime(1,2,10);
+
+            System.out.println("\n\nMY MANUAL SCHEDULE GANTT:" + manualSchedule.asciiGantt());
+
 
             ResourceOrder manualRO = new ResourceOrder(instance);
-            // TODO: encode the same solution
-            //manualRO.addTaskToMachine(..., new Task(..., ...));
+            manualRO.addTaskToMachine(0, new Task(0,0));
+            manualRO.addTaskToMachine(1, new Task(0,1));
+            manualRO.addTaskToMachine(2, new Task(0,2));
+            manualRO.addTaskToMachine(1, new Task(1,0));
+            manualRO.addTaskToMachine(0, new Task(1,1));
+            manualRO.addTaskToMachine(2, new Task(1,2));
 
+            System.out.println("\n\nMY MANUAL RESOURCE ORDER GANTT:" + manualRO.toSchedule().get().asciiGantt());
+
+
+            ResourceOrder optimalRO = new ResourceOrder(instance);
+            optimalRO.addTaskToMachine(0, new Task(0,0));
+            optimalRO.addTaskToMachine(1, new Task(1,0));
+            optimalRO.addTaskToMachine(1, new Task(0,1));
+            optimalRO.addTaskToMachine(0, new Task(1,1));
+            optimalRO.addTaskToMachine(2, new Task(1,2));
+            optimalRO.addTaskToMachine(2, new Task(0,2));
+
+            System.out.println("\n\nMY OPTIMAL RESOURCE ORDER GANTT:" + optimalRO.toSchedule().get().asciiGantt());
+
+        /*
+            // Bellow is the code that can crash the app
+            ResourceOrder crashRO = new ResourceOrder(instance);
+            crashRO.addTaskToMachine(0, new Task(0,1));
+
+            // We changed above task to try putting on machine 0 a wrong task.
+            // As the solution we are building no longer satisfies the constraints of the instance,
+            // the program crashes producing a RunTimeException when trying to schedule Task(0,1) on machine 0
+
+            crashRO.addTaskToMachine(1, new Task(1,0));
+            crashRO.addTaskToMachine(1, new Task(0,1));
+            crashRO.addTaskToMachine(0, new Task(1,1));
+            crashRO.addTaskToMachine(2, new Task(1,2));
+            crashRO.addTaskToMachine(2, new Task(0,2));
+        */
+
+            
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
